688f85a8f6a0af74095d1e6d816805511159716d
JORGE PATRICK <jorgepatrickdev@gmail.com>
2023-03-28 13:34:47 -0300

    Testando

---
+++ src/MeuFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class MeuFrame extends JFrame {
+    public MeuFrame() {
+
+        super("Meu Frame");
+
+        JButton botao = new JButton("Clique");
+
+        getContentPane().add(botao);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300,300);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new MeuFrame();
+    }
+}
